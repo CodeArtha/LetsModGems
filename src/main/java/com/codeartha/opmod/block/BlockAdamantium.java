52f4d5dee4726abd6637c70450e802efe0ca82e2
unknown <codeartha@gmail.com>
2014-07-10 10:56:24 +0200

    First block + mapColor vid

--- src/main/java/com/codeartha/opmod/block/BlockAdamantium.java
+++ src/main/java/com/codeartha/opmod/block/BlockAdamantium.java
@@ -6,20 +6,23 @@ import net.minecraft.block.material.Material;
 public class BlockAdamantium extends BlockOP
 {
     private final MapColor mapColor;
-    private static final int COLOR_ID = 30;
+
+    // Sorry.. Used it in my video, but turns out it wasn't used at all, seems like getMapColor does not use it's parameter!
+    //private static final int COLOR_ID = 30;
 
     public BlockAdamantium(MapColor Color)
     {
         super();
         this.setBlockName("adamantiumBlock");
-        this.setHardness( 20.0f );
-        this.setResistance( 2500.0f );
+        this.setHardness( 20.0f ); // Mining speed, ae: Osidian = 50.0f ; Iron Block = 5.0f
+        this.setResistance( 2500.0f ); // Blast resistance, ae: Obsidian : 2000.0f
         this.setStepSound( soundTypeMetal );
         this.mapColor = Color;
 
     }
 
-    public MapColor getMapColor(int COLOR_ID)
+    // /!\ I removed COLOR_ID ▼ Turns out it isn't used :)
+    public MapColor getMapColor()
     {
         return this.mapColor;
     }
